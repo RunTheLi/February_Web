e97e718c626c865248d9d9ce7d6106344cd7c8c6
RunTheLi <lanmarock@gmail.com>
2025-01-31 12:28:37 +0700

    Deployment commit

---
+++ 4a61a23338d15df95943.js
@@ -0,0 +1,21 @@
+export default function initAnimations() {
+    $(document).on("mouseenter", ".container", function () {
+        $(".card").stop().animate({ top: "-90px" }, "slow");
+    });
+
+    $(document).on("mouseleave", ".container", function () {
+        $(".card").stop().animate({ top: "5px" }, "slow");
+    });
+
+    $(document).on("click", ".valentines", function () {
+        $(".hearts .one").stop().animate({ top: "-150px", opacity: 0 }, 1000);
+        $(".hearts .two").stop().animate({ top: "-150px", opacity: 0 }, 1500);
+        $(".hearts .three").stop().animate({ top: "-150px", opacity: 0 }, 2000);
+        $(".hearts .four").stop().animate({ top: "-150px", opacity: 0 }, 2500);
+        $(".hearts .five").stop().animate({ top: "-150px", opacity: 0 }, 3000);
+    });
+
+    $(document).on("animationend", ".hearts div", function () {
+        $(this).css({ opacity: 1, top: "50px" });
+    });
+}
